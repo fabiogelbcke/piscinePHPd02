3886f95625e7ca95c00300c15b7e170d52fced9e
Fabio Gelbcke <dietbacon@Fabios-MBP.lan>
2015-11-05 14:14:30 +0100

    ex02 fixed and much more elegant, dare I say

--- ex02/loupe.php
+++ ex02/loupe.php
@@ -1,10 +1,15 @@
 #!/usr/bin/php
 <?php
+        function textos($matches)
+        {
+                $up = preg_replace_callback("/>.*</sU", function($matches){return (strtoupper($matches[0]));}, $matches[0]);
+                $up = preg_replace_callback('/title="(.*)"/sU', function($matches){return ('title="'.strtoupper($matches[1]).'"');}, $up);
+                return $up;
+        }
         if ($argc == 1)
            exit("\n");
         $myfile = fopen($argv[1], "r") or die("Unable to open file!");
         $filetxt = fread($myfile, filesize($argv[1]));
-        $filetxt = preg_replace_callback("/(<a[^>]*>)([^<]*)/", function($matches){return ($matches[1].strtoupper($matches[2]));}, $filetxt);
-        $filetxt = preg_replace_callback('/(<a.*)(title=")([^"]*")(.*<\/a>)?/', function($matches){return ($matches[1].$matches[2].strtoupper($matches[3]).$matches[4]);}, $filetxt);        
+        $filetxt = preg_replace_callback("/(<a[^>]*)(.*)(\/a>)/sU", "textos", $filetxt);
         echo $filetxt;
 ?>
